f3179c3b24a3bf74682089ca8ee4495f64853149
Sherifsalam <sherifehab205@icloud.com>
2023-11-15 08:11:14 -0800

    n

---
+++ enviroment.c
@@ -0,0 +1,92 @@
+#include "shell.h"
+
+/**
+ * _myenv - prints the current environment
+ * @information: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int _myenv(info_t *information)
+{
+	print_list_str(information->env);
+	return (0);
+}
+
+/**
+ * _getenv - gets the value of an environ variable
+ * @information: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *information, const char *name)
+{
+	list_t *node = information->env;
+	char *q;
+
+	while (node)
+	{
+		q = starts_with(node->str, name);
+		if (q && *q)
+			return (q);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _mysetenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @information: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _mysetenv(info_t *information)
+{
+	if (information->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, information->argv[1], information->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _myunsetenv - Remove an environment variable
+ * @information: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _myunsetenv(info_t *information)
+{
+	int j;
+
+	if (information->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (j = 1; j <= information->argc; j++)
+		_unsetenv(info, information->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env linked list
+ * @information: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int populate_env_list(info_t *information)
+{
+	list_t *node = NULL;
+	size_t j;
+
+	for (j = 0; environ[j]; i++)
+		add_node_end(&node, environ[j], 0);
+	information->env = node;
+	return (0);
+}
